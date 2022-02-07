2a637a8ecbfb8d3f801943462f548efb8687edce
Rene Klacan <Rene1998@yandex.com>
2022-02-07 10:22:14 +0100

    initial commit

---
+++ crawlers/goalComCrawler.js
@@ -0,0 +1,67 @@
+import axios from 'axios'
+import pretty from 'pretty'
+import * as cheerio from 'cheerio'
+
+export const getGoalNews = async(pageNum) => {
+	if(!pageNum) pageNum = 1 
+	const url = `${process.env.WS_GOALCOM}en/news/${pageNum}`
+	const fetchedContent = []
+
+	await axios.get(url).then((res) => {
+		const $ = cheerio.load(res.data)
+
+		$('.page-container').children().children().each((i, elem) => {
+			const card = $(elem).find('tbody tr')
+			const content = {
+				title: card.find('h3').text(),
+				img: card.find('td a img').attr('src'),
+				tag: card.find('.widget-news-card__content .widget-news-card__category').text(),
+				article_link: card.find('td a').attr('href')
+			}
+
+			let isValid = true
+
+			Object.values(content).forEach((contentValue) => {
+				if(!contentValue) {
+					isValid = false
+					console.log('Empty card content, returning...')
+				}
+			})
+
+			// const _parseByArgs = (prefix) => {
+			// 	if(!card_link.includes(prefix)) return
+			// 	if(card_link.includes('https://www.goal.com/en/match/') && (prefix == '/en/match/' || '/en/news/live/')) return
+			// 	console.log(card_link.replace(prefix, '').split('/').length > 2 && card_link)
+			// 	content.prefix = prefix
+			// 	[ content.article_slug, content.article_id ] = card_link.replace(prefix, '').split('/')
+			// }
+
+			isValid && fetchedContent.push(content)
+		})
+
+		const timeout = process.env.WS_GOALCOM_LOAD_USE_INTERVAL == 'true' 
+			? Math.floor(Math.random() * Number(process.env.WS_GOALCOM_LOAD_END_INTERVAL)) + Number(process.env.WS_GOALCOM_LOAD_INITIAL_INTERVAL)
+			: 0
+
+		console.log(`successfully crawled ${url}, ${process.env.WS_GOALCOM_LOAD_MULTIPLE == 'true' ? `next fetch in ${timeout}ms` : 'multiple disabled'}`)
+
+		process.env.WS_GOALCOM_LOAD_MULTIPLE == 'true'
+			&& pageNum < Number(process.env.WS_GOALCOM_MAX_NUM_OF_PAGES)
+				? setTimeout(async () => await getGoalNews(pageNum+1), timeout)
+				: console.log('scraping finished')
+		
+	}).catch((err) => 
+		console.error(`Could not fetch ${process.env.WS_GOALCOM}en/news/${pageNum} failed with ${err}`)
+	)
+	return fetchedContent
+}
+
+export const getGoalArticleByUrl = async (article_link) => {
+	const url = `${article_link.startsWith('/en') ? `${process.env.WS_GOALCOM}${article_link}` : article_link}`
+	console.log(url)
+
+	axios.get(url).then((res) => {
+		const $ = cheerio.load(res.data)
+		console.log(pretty($.html()))
+	})
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+import { getGoalNews, getGoalArticleByUrl } from './crawlers/goalComCrawler.js';
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT ,() => console.log(`server has started successfully at port :${process.env.PORT}`))
+let goalNewsContent = []
+
+const initCrawlers = async () => {
+	goalNewsContent = await getGoalNews()
+	// await getGoalArticle('afcon-2021-burkina-faso-vs-senegal-preview-kick-off-time-tv', 'blte188a9a5d9fa55a0')
+}
+
+initCrawlers()
+
+app.get('/goal-com/fetched-news', (req, res) => res.json(goalNewsContent))
+
+app.post('/goal-com/fetched-article', async (req, res) => {
+	console.log(req.body)
+	const article = await getGoalArticleByUrl(req.body.url)
+	res.json(article)
+})
